41c979507eaaa8eba6e79c33465090ef7ee8a0ca
sananaser <sananaser066@gmail.com>
2026-06-04 12:42:43 +0530

    Mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const dotenv = require("dotenv");
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 dotenv.config();
 
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
